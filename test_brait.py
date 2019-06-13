db5c8d4bec1ebeb3756079e69930cc7901b76471
Clemence <clemence.vast@mines-nancy.org>
2019-06-13 18:49:01 +0200

    nouvelle scene et changement de tous les capteurs

--- test_brait.py
+++ test_brait.py
@@ -11,17 +11,17 @@ class Obstacle_test:
         self.robot = robot
 
         self.alpha = [1/6,1/6,1/(math.pi)]  # normalition avec limite du monde cartesien = -3m � + 3m
-        
+
     def avoid_obst(self):
-        noDetectionDist=0.8
-        maxDetectionDist=0.5
+        noDetectionDist=0.1
+        maxDetectionDist=0.04
         detect=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #why are all the values zero, what does this do?
-        braitenbergL=[-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, -0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6] # why are the values like this, how is it calculated
-        braitenbergR=[-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, -1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2]
+        braitenbergL=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8, 0,0,0,0,0,0,0,0] # why are the values like this, how is it calculated
+        braitenbergR=[0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1, 0,0,0,0,0,0,0,0]
         v0=1 # what is v0 and why is it set to 2, does it mean a velocity of 2m/s or something
 
-        list_ind,list_dist,list_state=self.robot.get_obstacle()  
-        
+        list_ind,list_dist,list_state=self.robot.get_obstacle()
+
         for i in range(len(list_ind)):
             dist=list_dist[i]
             if(list_state[i] and dist<noDetectionDist):
@@ -30,17 +30,17 @@ class Obstacle_test:
                     dist=maxDetectionDist
                 detect[list_ind[i]]=1-((dist-maxDetectionDist)/(noDetectionDist-maxDetectionDist))
     #   simHandleChildScript(sim_handle_all_except_explicit)
-    
+
         vLeft=v0
         vRight=v0
-    
+
         for i in range(0,16):
             vLeft=vLeft+braitenbergL[i]*detect[i]
             vRight=vRight+braitenbergR[i]*detect[i]
-    
+
         self.robot.set_motor_velocity([vLeft,vRight])
         self.running = False
-        
+
     def moove(self,iter):
         for i in range(iter):
             self.avoid_obst()
