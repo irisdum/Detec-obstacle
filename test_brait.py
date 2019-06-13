52884c6462e5f422c323dcf3bee45f79757b8841
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2019-06-13 18:51:26 +0200

    minor modif

--- test_brait.py
+++ test_brait.py
@@ -13,17 +13,17 @@ class Obstacle_test:
         self.alpha = [1/6,1/6,1/(math.pi)]  # normalition avec limite du monde cartesien = -3m � + 3m
         
     def avoid_obst(self):
-        noDetectionDist=0.8
-        maxDetectionDist=0.5
-        detect=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #why are all the values zero, what does this do?
-        braitenbergL=[-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, -0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6] # why are the values like this, how is it calculated
-        braitenbergR=[-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, -1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2]
+        noDetectionDist=0.5
+        maxDetectionDist=0.2
+        detect=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #
+        braitenbergL=[-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0] 
+       braitenbergR=[-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
         v0=1 # what is v0 and why is it set to 2, does it mean a velocity of 2m/s or something
 
         list_ind,list_dist,list_state=self.robot.get_obstacle()  
         
         for i in range(len(list_ind)):
-            dist=list_dist[i]
+            dist=list_dist[list_ind[i]-1]
             if(list_state[i] and dist<noDetectionDist):
                 print('un objet', str(list_ind[i]))
                 if (dist<maxDetectionDist) :
